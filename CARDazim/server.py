7e745e37596201a7f7575d75c24f0ea8f7744134
Omri Tabrikian <tabrikian@mail.tau.ac.il>
2024-06-04 23:52:29 +0300

    Added client server

---
+++ CARDazim/server.py
@@ -0,0 +1,45 @@
+import argparse
+import sys
+import socket
+
+
+def run_server(server_ip, server_port):
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((server_ip, server_port))
+    server.listen(5)
+    while True:
+        conn, addr = server.accept()
+        from_client = ''
+        while True:
+            data = conn.recv(4096)
+            if not data: break
+            from_client += data.decode('utf8')
+            print(f'Received data: {from_client}')
+        conn.close()
+
+
+def get_args():
+    parser = argparse.ArgumentParser(description='Send data to server.')
+    parser.add_argument('server_ip', type=str,
+                        help='the server\'s ip')
+    parser.add_argument('server_port', type=int,
+                        help='the server\'s port')
+    return parser.parse_args()
+
+
+def main():
+    '''
+    Implementation of CLI and sending data to server.
+    '''
+    args = get_args()
+    try:
+        print("server is ready")
+        run_server(args.server_ip, args.server_port)
+        print('Done.')
+    except Exception as error:
+        print(f'ERROR: {error}')
+        return 1
+
+
+if __name__ == '__main__':
+    sys.exit(main())
