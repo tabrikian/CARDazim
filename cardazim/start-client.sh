02ae69230754f7c895c41c55537e783fb39cc9c9
Omri Tabrikian <tabrikian@mail.tau.ac.il>
2024-06-18 19:13:31 +0300

    add start files

---
+++ cardazim/start-client.sh
@@ -0,0 +1,3 @@
+python -m pip install Pillow
+pip install PyCryptodome
+python3 ./client.py 127.0.0.1 12345 "panda" "Omri" "/home/user/Pictures/panda.jpg" "who?" "po"
---
+++ cardazim/start-server.sh
@@ -0,0 +1,5 @@
+pip install flask
+pip install pymongo
+python -m pip install Pillow
+pip install PyCryptodome
+python3 ./api_server.py 127.0.0.1 12345 "mongodb+srv://omri:Omrirmo1@cluster0.auqkyxp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
